97799922ac9111fb2b51e91d15afcf291fd59248
brandonocasey <brandonocasey@gmail.com>
2018-12-03 16:20:58 -0500

    fix make seek-to-live accessible

--- src/js/control-bar/seek-to-live.js
+++ src/js/control-bar/seek-to-live.js
@@ -39,11 +39,11 @@ class SeekToLive extends Button {
       className: 'vjs-seek-to-live-control vjs-control'
     });
 
-    this.contentEl_ = Dom.createEl('div', {
-      className: 'vjs-seek-to-live',
-      innerHTML: `<span class="vjs-control-text">${this.localize('Stream Type')}\u00a0</span>${this.localize('LIVE')}`
+    this.textEl_ = Dom.createEl('span', {
+      className: 'vjs-seek-to-live-text',
+      innerHTML: this.localize('LIVE')
     }, {
-      'aria-live': 'off'
+      'aria-hidden': 'true'
     });
 
     this.circleEl_ = Dom.createEl('span', {
@@ -51,7 +51,7 @@ class SeekToLive extends Button {
     });
 
     el.appendChild(this.circleEl_);
-    el.appendChild(this.contentEl_);
+    el.appendChild(this.textEl_);
     return el;
   }
 
@@ -61,9 +61,13 @@ class SeekToLive extends Button {
    */
   updateLiveEdgeStatus(e) {
     if (this.player_.liveTracker.behindLiveEdge()) {
+      this.setAttribute('aria-disabled', false);
       this.removeClass('vjs-at-live-edge');
+      this.controlText('Seek To LIVE');
     } else {
       this.addClass('vjs-at-live-edge');
+      this.controlText('At LIVE edge');
+      this.setAttribute('aria-disabled', true);
     }
   }
 
@@ -81,13 +85,14 @@ class SeekToLive extends Button {
    */
   dispose() {
     this.off(this.player_.liveTracker, 'liveedgechange', this.updateLiveEdgeStatus);
-    this.contentEl_ = null;
+    this.textEl_ = null;
     this.circleEl_ = null;
 
     super.dispose();
   }
-
 }
+
+SeekToLive.prototype.controlText_ = 'Seek to LIVE';
 
 Component.registerComponent('SeekToLive', SeekToLive);
 export default SeekToLive;
